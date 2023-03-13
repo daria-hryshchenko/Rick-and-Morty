bade012f79146263ffd36e7921e410e2e6b25594
Daria Hryshchenko <daria199210@gmail.com>
2023-03-13 11:10:56 +0200

    install react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
+import React, { Suspense } from 'react';
 import { Container } from './App.styled';
+import Loader from 'components/Loader/Loader';
 
 export const App = () => {
-  return <Container>fff</Container>;
+  return (
+    <Container>
+      <Suspense fallback={<Loader />}></Suspense>
+    </Container>
+  );
 };
