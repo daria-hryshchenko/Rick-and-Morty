6345a861bccdb2eea96171cdcee337d34ee46343
Daria Hryshchenko <98493900+daria-hryshchenko@users.noreply.github.com>
2023-03-15 01:02:32 +0200

    change basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter
-    // basename="/Rick-and-Morty/"
+    basename="/Rick-and-Morty"
     >
       <App />
     </BrowserRouter>
