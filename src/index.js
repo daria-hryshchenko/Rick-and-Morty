d2c0725d029e709489677cb2b392a6224e49bc84
Daria Hryshchenko <daria199210@gmail.com>
2023-03-19 18:52:31 +0200

    add dotenv

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-
 import GlobalStyle from 'GlobalStyles/GlobalStyles.jsx';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Rick-and-Morty/">
+    <BrowserRouter
+    // basename="/Rick-and-Morty/"
+    >
       <GlobalStyle />
       <App />
     </BrowserRouter>
