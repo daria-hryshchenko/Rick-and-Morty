5ece94637f4e8d678b44370ce30d615a4de8aaef
Daria Hryshchenko <daria199210@gmail.com>
2023-03-19 22:48:09 +0200

    try to add var github

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import GlobalStyle from 'GlobalStyles/GlobalStyles.jsx';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
